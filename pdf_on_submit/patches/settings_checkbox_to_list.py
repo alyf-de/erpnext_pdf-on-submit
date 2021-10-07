eb162feaf34269dd32704b2f533544e8457e50ae
barredterra <14891507+barredterra@users.noreply.github.com>
2021-10-07 12:14:28 +0200

    feat: patch changes in settings
    (cherry picked from commit 2497ff4cb1191a8751b5f960126e959b08c4ee9f)

---
+++ pdf_on_submit/patches/settings_checkbox_to_list.py
@@ -0,0 +1,19 @@
+import frappe
+from frappe import scrub
+from frappe.utils import cint
+
+
+def execute():
+    """
+    Change PDF on Submit Settings from checkboxes for some doctypes to a list
+    of enabled doctypes.
+    """
+    frappe.reload_doc("pdf_on_submit", "doctype", "enabled_doctype")
+    frappe.reload_doc("pdf_on_submit", "doctype", "pdf_on_submit_settings")
+
+    settings = frappe.get_single("PDF on Submit Settings")
+    for dt in ("Quotation", "Sales Order", "Sales Invoice", "Dunning", "Delivery Note"):
+        if cint(settings.get(scrub(dt))):
+            settings.append("enabled_for", {"document_type": dt})
+
+    settings.save()
