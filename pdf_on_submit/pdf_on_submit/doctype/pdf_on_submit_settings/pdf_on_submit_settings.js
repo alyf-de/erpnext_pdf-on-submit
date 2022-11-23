9b45639c1d667d15c15136674784c79449765070
barredterra <14891507+barredterra@users.noreply.github.com>
2022-11-23 20:27:47 +0100

    feat: add support for new print format builder

--- pdf_on_submit/pdf_on_submit/doctype/pdf_on_submit_settings/pdf_on_submit_settings.js
+++ pdf_on_submit/pdf_on_submit/doctype/pdf_on_submit_settings/pdf_on_submit_settings.js
@@ -1,8 +1,14 @@
 // Copyright (c) 2019, Raffael Meyer and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('PDF on Submit Settings', {
-	refresh: function(frm) {
-
-	}
+frappe.ui.form.on("PDF on Submit Settings", {
+	refresh(frm) {
+		frm.set_query("print_format", "enabled_for", function(doc, cdt, cdn) {
+			return {
+				filters: {
+					doc_type: locals[cdt][cdn].document_type,
+				},
+			};
+		});
+	},
 });
