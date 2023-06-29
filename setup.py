6d02ee082dd57320e206a144dc3dd1f33ed52092
barredterra <14891507+barredterra@users.noreply.github.com>
2023-06-29 18:54:05 +0200

    refactor: setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,18 @@
 # -*- coding: utf-8 -*-
-from setuptools import setup, find_packages
-import re, ast
+from pdf_on_submit import __version__ as version
+from setuptools import find_packages, setup
 
-with open('requirements.txt') as f:
-	install_requires = f.read().strip().split('\n')
-
-# get version from __version__ variable in pdf_on_submit/__init__.py
-_version_re = re.compile(r'__version__\s+=\s+(.*)')
-
-with open('pdf_on_submit/__init__.py', 'rb') as f:
-	version = str(ast.literal_eval(_version_re.search(
-		f.read().decode('utf-8')).group(1)))
+with open("requirements.txt") as f:
+	install_requires = f.read().strip().split("\n")
 
 setup(
-	name='pdf_on_submit',
+	name="pdf_on_submit",
 	version=version,
-	description='Generatation, Sales Order, Sales Invoice and Delivery Note',
-	author='Raffael Meyer',
-	author_email='raffael@alyf.de',
+	description="Print to PDF on submit of a doctype",
+	author="ALYF GmbH",
+	author_email="hallo@alyf.de",
 	packages=find_packages(),
 	zip_safe=False,
 	include_package_data=True,
-	install_requires=install_requires
+	install_requires=install_requires,
 )
