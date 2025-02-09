f38decfce30cf935b9b2e0388a67ce9698080780
barredterra <14891507+barredterra@users.noreply.github.com>
2025-02-09 18:52:34 +0100

    feat: validate filters

--- pdf_on_submit/pdf_on_submit/doctype/enabled_doctype/enabled_doctype.py
+++ pdf_on_submit/pdf_on_submit/doctype/enabled_doctype/enabled_doctype.py
@@ -1,7 +1,17 @@
 # Copyright (c) 2021, Raffael Meyer and contributors
 # For license information, please see license.txt
+import json
+
+import frappe
 from frappe.model.document import Document
+from frappe.utils.data import evaluate_filters
 
 
 class EnabledDocType(Document):
-	pass
+	def validate_filters(self):
+		if not self.filters:
+			return
+
+		filters = json.loads(self.filters)
+		dummy_doc = frappe.new_doc(self.document_type)
+		evaluate_filters(dummy_doc, filters)
--- pdf_on_submit/pdf_on_submit/doctype/pdf_on_submit_settings/pdf_on_submit_settings.py
+++ pdf_on_submit/pdf_on_submit/doctype/pdf_on_submit_settings/pdf_on_submit_settings.py
@@ -1,7 +1,19 @@
 # Copyright (c) 2019, Raffael Meyer and contributors
 # For license information, please see license.txt
+import frappe
+from frappe import _
 from frappe.model.document import Document
 
 
 class PDFonSubmitSettings(Document):
-	pass
+	def validate(self):
+		for enabled_doctype in self.enabled_for:
+			try:
+				enabled_doctype.validate_filters()
+			except Exception as e:
+				frappe.clear_messages()
+				frappe.throw(
+					_("Row #{0}: invalid filters for <b>{1}</b>: {2}").format(
+						enabled_doctype.idx, enabled_doctype.document_type, e
+					)
+				)
