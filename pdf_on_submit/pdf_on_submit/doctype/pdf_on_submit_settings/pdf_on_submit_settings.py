cf1f1a93c0d80e62474fe72bda2e219c6ea34e37
Patrick Eissler <77415730+PatrickDEissler@users.noreply.github.com>
2025-05-30 15:10:26 +0200

    fix: validate attach to fields (no duplicates and only valid fields)

--- pdf_on_submit/pdf_on_submit/doctype/pdf_on_submit_settings/pdf_on_submit_settings.py
+++ pdf_on_submit/pdf_on_submit/doctype/pdf_on_submit_settings/pdf_on_submit_settings.py
@@ -17,3 +17,41 @@ class PDFonSubmitSettings(Document):
 						enabled_doctype.idx, enabled_doctype.document_type, e
 					)
 				)
+
+		self.validate_attach_to_fields()
+
+	def validate_attach_to_fields(self):
+		"""
+		Validates:
+		1) attach_to_field is not processed multiple times in one document
+		2) that the `attach_to_field` is a valid field in the DocType.
+		Note: The 2nd validation would be more robust (but less performant), if done for each transaction.
+		"""
+		attach_to_fields = [
+			(enabled_doctype.document_type, enabled_doctype.attach_to_field) for enabled_doctype in self.enabled_for
+			if enabled_doctype.attach_to_field
+		]
+		if attach_to_fields:
+			_avoid_fields_being_processed_multiple_times(attach_to_fields)
+			_check_if_attach_to_fields_are_valid(attach_to_fields)
+
+
+def _avoid_fields_being_processed_multiple_times(attach_to_fields):
+	seen_fields = set()
+	for pair in attach_to_fields:
+		if pair in seen_fields:
+			frappe.throw(
+				_("It is not allowed to set the attach field {0} in the DocType {1} multiple times.").format(pair[1], pair[0])
+			)
+		seen_fields.add(pair)
+
+
+def _check_if_attach_to_fields_are_valid(attach_to_fields):
+	for doctype, fieldname in attach_to_fields:
+		if (
+			not frappe.db.exists("DocField", {"parent": doctype, "fieldname": fieldname, "fieldtype": "Attach"})
+			and not frappe.db.exists("Custom Field", {"dt": doctype, "fieldname": fieldname, "fieldtype": "Attach"})
+		):
+			frappe.throw(
+				_("The field {0} in the DocType {1} is not a valid field.").format(fieldname, doctype)
+			)
