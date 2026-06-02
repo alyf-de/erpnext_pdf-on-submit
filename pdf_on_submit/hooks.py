dce6c6730c3bd7e2e747d707153a31f82cc6d972
Henning Wendtland <156231187+HenningWendtland@users.noreply.github.com>
2026-06-02 12:14:17 +0200

    fix: reinsert deleted comments in hooks.py

--- pdf_on_submit/hooks.py
+++ pdf_on_submit/hooks.py
@@ -25,7 +25,8 @@ extend_bootinfo = "pdf_on_submit.utils.extend_boot_info"
 
 # include js in page
 # page_js = {"page" : "public/js/file.js"}
-
+# include js in doctype views
+# doctype_js = {"doctype" : "public/js/doctype.js"}
 # doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
 # doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
