69fa90f7ac5d6f20de68e5a5440c34268aa24eb0
barredterra <14891507+barredterra@users.noreply.github.com>
2026-06-04 18:05:15 +0200

    ci: drop unused db-user setup

--- .github/helper/install.sh
+++ .github/helper/install.sh
@@ -21,10 +21,6 @@ bench init --skip-assets --frappe-path ~/frappe --python "$(which python)" frapp
 mysql --host 127.0.0.1 --port 3306 -u root -proot -e "SET GLOBAL character_set_server = 'utf8mb4'"
 mysql --host 127.0.0.1 --port 3306 -u root -proot -e "SET GLOBAL collation_server = 'utf8mb4_unicode_ci'"
 
-mysql --host 127.0.0.1 --port 3306 -u root -proot -e "CREATE USER 'test_frappe'@'localhost' IDENTIFIED BY 'test_frappe'"
-mysql --host 127.0.0.1 --port 3306 -u root -proot -e "CREATE DATABASE test_frappe"
-mysql --host 127.0.0.1 --port 3306 -u root -proot -e "GRANT ALL PRIVILEGES ON \`test_frappe\`.* TO 'test_frappe'@'localhost'"
-
 mysql --host 127.0.0.1 --port 3306 -u root -proot -e "FLUSH PRIVILEGES"
 
 cd ~/frappe-bench || exit
