5095f3bd0bbadca8d2f24ad6ba823707c2253fae
barredterra <14891507+barredterra@users.noreply.github.com>
2026-06-05 15:42:30 +0200

    style: format python (2)

--- pdf_on_submit/utils.py
+++ pdf_on_submit/utils.py
@@ -5,7 +5,6 @@ from pdf_on_submit.attach_pdf import iter_matching_enabled_doctypes
 
 
 def extend_boot_info(bootinfo):
-
 	settings = frappe.get_single("PDF on Submit Settings")
 
 	show_pdf_button = cint(settings.show_pdf_button)
