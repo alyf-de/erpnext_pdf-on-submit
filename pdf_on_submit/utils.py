fa0251011c4ab79c3d384159e98bc85a154560eb
HenningWendtland <156231187+HenningWendtland@users.noreply.github.com>
2026-06-26 09:28:35 +0200

    chore: rerun pre-commit

--- pdf_on_submit/utils.py
+++ pdf_on_submit/utils.py
@@ -11,9 +11,7 @@ def extend_boot_info(bootinfo):
 	bootinfo.pdf_on_submit = frappe._dict(
 		{
 			"show_pdf_button": show_pdf_button,
-			"enabled_doctypes": list(
-				{row.document_type for row in settings.enabled_for if row.document_type}
-			)
+			"enabled_doctypes": list({row.document_type for row in settings.enabled_for if row.document_type})
 			if show_pdf_button
 			else [],
 		}
